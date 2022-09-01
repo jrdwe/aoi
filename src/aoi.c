1ab4ab218c355157fa260efb0189791215788ac7
Jarryd <jarryd.webb@hotmail.com>
2022-09-01 23:04:26 +1000

    include 'help' & 'exit' builtin commands

--- src/aoi.c
+++ src/aoi.c
@@ -1,15 +1,71 @@
 #include "aoi.h"
 
-char** 
-parseinput(char* line)
+const char *builtin_commands[] =
 {
-	exit(EXIT_FAILURE);
+	"help",
+	"exit"
+};
+
+void (*builtin_functions[]) (char **) =
+{
+	&builtin_help,
+	&builtin_exit
+};
+
+int 
+builtin_count(void)
+{
+	return sizeof(builtin_commands) / sizeof(char *);
+}
+
+void 
+builtin_help(char** argument)
+{
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("Welcome! You've called the 'help' command.\n");
+	printf("This program is a simple shell with a few builtin commands\n\n");
+	printf("These commands are listed below: \n");
+
+	for (int i = 0; i < builtin_count(); i++)
+		printf("++ %s\n", builtin_commands[i]);
+
+	printf("\nAll other valid system commands and arguments can be used as well.\n");
+	printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+}
+
+void 
+builtin_exit(char** argument)
+{
+	printf("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+	printf("See you soon! Exiting now..\n");
+
+	exit(EXIT_SUCCESS);
+}
+
+char** 
+parse_input(char* line)
+{
+	int ctr = 0;
+	char *arg;
+	char **args = malloc(sizeof(*args) * AOI_ARGS_BUFSIZE);
+
+	// check malloc has successfully allocated memory space
+	if (!args)
+	{
+		fprintf(stderr, "Unable to allocate required memory for arguments\n");
+		exit(EXIT_FAILURE);	
+	}
+
+	while ((arg = strsep(&line, " ")) != NULL)
+		args[ctr++] = arg;
+
+	return args;
 }
 
 char*
-readinput(void)
+read_input(void)
 {
-	char* line = NULL;
+	char *line = NULL;
 	size_t buflen = 0;
 	size_t linelen;
 	
@@ -19,15 +75,31 @@ readinput(void)
 	exit(EXIT_FAILURE);
 }
 
+void
+userloop(void)
+{
+	for(;;)
+	{
+		printf("--> ");
+		char* user_input = read_input();
+
+		for (int i = 0; i < builtin_count(); i++)
+		{
+			if (strcmp(parse_input(user_input)[0], builtin_commands[i]) == 0)
+				builtin_functions[i](parse_input(user_input));
+		}
+	}
+}
+
 int
 main(void)
 {
-	// Step 1: Read in user input from stdin
-	// Step 2: Parse user input from stdin
-	// Step 3: Exec command with the provided arguments
-	
-	printf("String: %s", readinput());
-	
+	// Todo
+	// Exec command with the provided arguments
+	// Welcome art! 
+	// Trim function to remove whitespace
+
+	userloop();	
 
 	exit(EXIT_SUCCESS);
 }
--- src/aoi.h
+++ src/aoi.h
@@ -1,8 +1,8 @@
 /** @file aoi.h
- *  @brief Function prototypes for the simple shell.
+ *  @brief Function prototypes for aoi, the simple shell.
  *
- *  This contains the prototypes for a simple shell
- *  alongwith all constants, and global variables you need.
+ *  This contains the prototypes for the project 
+ *  alongwith all constants, and global variables.
  *
  *  @author Jarryd W (jrdwe)
  *  @bug No known bugs.
@@ -16,12 +16,35 @@
 #include <string.h>
 
 #define AOI_ARGS_BUFSIZE 32
-#define AOI_ARGS_DELIM " " 
+#define AOI_ARGS_DELIM " \t\n" 
+
+// Variables for the built-in shell commands 
+extern const char *builtin_commands[];
+extern void (*builtin_functions[]) (char **);
+
+/* @brief Returns the number of builtin commands 
+ * 
+ * @return int 
+ */
+int builtin_count(void); 
+
+/* @brief Displays help and advice to the programs user
+ * 
+ * @return void
+ */
+void builtin_help(char** argument); 
+
+/* @brief Exits the program gracefully at the users request
+ * 
+ * @return void
+ */
+void builtin_exit(char** argument); 
 
 /* @brief Reads in a text-line from stdin 
+ *
  * @return A heap-allocated line 
  */
-char* readinput(void);
+char *read_input(void);
 
 /* @brief Seperates the user's input by whitespace 
  *				to acquire the desired commmand and any 
@@ -30,6 +53,13 @@ char* readinput(void);
  * @param	line The user's input
  * @return An array of strings taken from line
  */
-char** parseinput(char* line);
+char **parse_input(char* line);
+
+/* @brief The core program loop of the shell
+ *				which runs until the user exits
+ *
+ * @return void
+ */ 
+void userloop(void);
 
 #endif
